3357f53056f02b1e6e6fb8aa904aab63c442ddee
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-12-06 09:07:58 +0330

    refactor: lift userInput state up to App and convert onChange to handleChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInputs";
+import Result from "./components/Result";
+import { useState } from "react";
 function App() {
+  const initialUserInput = {
+    initialInvestment: 10000,
+    annualInvestment: 100,
+    expectedReturn: 20,
+    duration: 10,
+  };
+  const [userInput, setUserInput] = useState(initialUserInput);
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+        // '+' converts string to number
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Result />
     </>
   );
 }
--- src/components/UserInputs.jsx
+++ src/components/UserInputs.jsx
@@ -3,59 +3,46 @@ import { useState } from "react";
 import customTheme from "../themes/customInputTheme";
 import { ThemeProvider } from "@mui/material/styles";
 
-export default function UserInput() {
-  const initialUserInput = {
-    initialInvestment: 0,
-    annualInvestment: 0,
-    expectedReturn: 0,
-    duration: 0,
-  };
-  const [userInput, setUserInput] = useState(initialUserInput);
-
+export default function UserInput({ onChange, userInput }) {
   return (
     <ThemeProvider theme={customTheme}>
       <form
         action=""
-        className="p-4 max-w-lg my-8 mx-auto rounded-sm bg-linear-to-b
- from-[rgb(48,126,108)] to-[rgb(43,153,109)] "
+        className="p-4 max-w-2xl my-8 mx-auto rounded-sm bg-linear-to-b
+ from-[rgb(48,126,108)] to-[rgb(43,153,109)] shadow-2xl"
       >
-        <section className="grid grid-cols-2 justify-between gap-6 py-6 px-4">
+        <section className="grid grid-cols-2 justify-between gap-6 py-4 px-2">
           <TextField
             id="initial-investment"
             label="INITIAL INVESTMENT"
             variant="outlined"
+            required
             value={userInput.initialInvestment}
-            onChange={(e) =>
-              setUserInput({ ...userInput, initialInvestment: e.target.value })
-            }
-            className="text-white"
+            onChange={(e) => onChange("initialInvestment", e.target.value)}
           />
           <TextField
             id="annual-investment"
             label="ANNUAL INVESTMENT"
             variant="outlined"
             value={userInput.annualInvestment}
-            onChange={(e) =>
-              setUserInput({ ...userInput, annualInvestment: e.target.value })
-            }
+            required
+            onChange={(e) => onChange("annualInvestment", e.target.value)}
           />
           <TextField
             id="expected-return"
             label="EXPECTED RETURN"
             variant="outlined"
             value={userInput.expectedReturn}
-            onChange={(e) =>
-              setUserInput({ ...userInput, expectedReturn: e.target.value })
-            }
+            required
+            onChange={(e) => onChange("expectedReturn", e.target.value)}
           />
           <TextField
             id="duration"
             label="DEURATION"
             variant="outlined"
             value={userInput.duration}
-            onChange={(e) =>
-              setUserInput({ ...userInput, duration: e.target.value })
-            }
+            required
+            onChange={(e) => onChange("duration", e.target.value)}
           />
         </section>
       </form>
